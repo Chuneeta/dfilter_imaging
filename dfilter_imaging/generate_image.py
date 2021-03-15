7c70f505791e161d1edb0279b88a3a8009187693
Ridhima Nunhokee <ridhima@Ridhimas-MacBook-Pro.local>
2021-03-15 16:47:32 +0500

    Script to generate images

---
+++ dfilter_imaging/generate_image.py
@@ -0,0 +1,10 @@
+from beam_solver import imaging as im
+from beam_solver import fits_utils as ft
+
+class Create_Image():
+    def __init__(self, ms):
+        self.ms = ms
+
+    def generate_image():
+        pass
+    
