81aaef4b4423260272f744a00b4b31752370c36f
Ridhima Nunhokee <ridhima@Ridhimas-MacBook-Pro.local>
2021-03-29 11:07:29 +0500

    added unittests

--- dfilter_imaging/tests/test_simulate.py
+++ dfilter_imaging/tests/test_simulate.py
@@ -8,7 +8,6 @@ import os
 # MSFILE
 msfile = os.path.join(DATA_PATH, 'zen.2458114.31193.HH.xx.t0.ms')
 params_file = os.path.join(DATA_PATH, 'sim_params.txt')
-sky_outfile = os.path.join(DATA_PATH, 'skymodel.dat')
 
 class Test_Simulate():
     def test_init_ms(self):
@@ -30,23 +29,23 @@ class Test_Simulate():
         nt.assert_equal(len(params), 5)
         np.testing.assert_equal(params, np.array([ 50.25,  30.78,   1.  ,  -0.7 , 150.  ]))
     
-    def test_complist_input(self):
-        sim = sm.Simulate(msfile, sparams=params_file)
-        params = sim.read_sim_parameters()
-        sim = sim.create_casa_skymodel(sky_outfile)
-        nt.assert_true(os.path.exists(sky_outfile))
+    #def test_complist_input(self):
+    #    sim = sm.Simulate(msfile, sparams=params_file)
+    #    params = sim.read_sim_parameters()
+    #    sim = sim.create_casa_skymodel()
+    #    nt.assert_true(os.path.exists(sky_outfile))
     
     def test_complist_output(self):
         sim = sm.Simulate(msfile, sparams=params_file)
         params = sim.read_sim_parameters()
-        sim = sim.create_casa_skymodel(sky_outfile)
-        nt.assert_true(os.path.exists(sky_outfile.replace('.dat', '.cl')))
+        sim = sim.create_casa_skymodel()
+        nt.assert_true(os.path.exists(params_file.replace('.txt', '.cl')))
 
     def test_complist_flux(self):
         sim = sm.Simulate(msfile, sparams=params_file)
         params = sim.read_sim_parameters()
-        sim = sim.create_casa_skymodel(sky_outfile)
-        skymodel = sky_outfile.replace('.dat', '.cl')
+        sim = sim.create_casa_skymodel()
+        skymodel = params_file.replace('.txt', '.cl')
         sk = rs.Skymodel(skymodel)
         flux = sk.get_flux_values()
         input_params = np.loadtxt(params_file)
@@ -55,8 +54,8 @@ class Test_Simulate():
     def test_complist_sloc(self):
         sim = sm.Simulate(msfile, sparams=params_file)
         params = sim.read_sim_parameters()
-        sim = sim.create_casa_skymodel(sky_outfile)
-        skymodel = sky_outfile.replace('.dat', '.cl')
+        sim = sim.create_casa_skymodel()
+        skymodel = params_file.replace('.txt', '.cl')
         sk = rs.Skymodel(skymodel)
         src_loc = sk.get_src_loc()
         input_params = np.loadtxt(params_file)
@@ -66,8 +65,8 @@ class Test_Simulate():
     def test_complist_freq(self):
         sim = sm.Simulate(msfile, sparams=params_file)
         params = sim.read_sim_parameters()
-        sim = sim.create_casa_skymodel(sky_outfile)
-        skymodel = sky_outfile.replace('.dat', '.cl')
+        sim = sim.create_casa_skymodel()
+        skymodel = params_file.replace('.txt', '.cl')
         sk = rs.Skymodel(skymodel)
         freq = sk.get_freqs()
         input_params = np.loadtxt(params_file)
@@ -76,9 +75,10 @@ class Test_Simulate():
     def test_complist_sindex(self):
         sim = sm.Simulate(msfile, sparams=params_file)
         params = sim.read_sim_parameters()
-        sim = sim.create_casa_skymodel(sky_outfile)
-        skymodel = sky_outfile.replace('.dat', '.cl')
+        sim = sim.create_casa_skymodel()
+        skymodel = params_file.replace('.txt', '.cl')
         sk = rs.Skymodel(skymodel)
         sindex = sk.get_sindex()
         input_params = np.loadtxt(params_file)
         nt.assert_equal(sindex[0], input_params[3])
+
