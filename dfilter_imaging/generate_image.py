5a2972f87464cfd77e96d19cc7ce3a47f7351f86
Ridhima Nunhokee <ridhima@Ridhimas-MacBook-Pro.local>
2021-03-29 11:04:41 +0500

    added ia tool to remove the weird circle

--- dfilter_imaging/generate_image.py
+++ dfilter_imaging/generate_image.py
@@ -1,16 +1,25 @@
 from beam_solver import fits_utils as ft
 import casatasks as ctk
+import casatools as ct
 import os
 
 class Image():
     def __init__(self, msfile):
         self.msfile = msfile
 
-    def generate_mfs_image(self, imagename, cell='8arcmin', imsize=512, weighting='uniform', niter=0, start=0, stop=1023, threshold='0Jy', phasecenter=''):
-        ctk.tclean(self.msfile, imagename=imagename, cell=cell, imsize=imsize, weighting=weighting, spw='0:{}~{}'.format(start, stop), niter=niter, threshold=threshold, phasecenter=phasecenter)
-    
-    def to_fits(self, imagename, fitsname):
-        ctk.exportfits(imagename=imagename, fitsimage=fitsname)
+    def generate_mfs_image(self, imagename, cell='8arcmin', imsize=512, weigh:wting='uniform', niter=0, start=0, stop=1023, threshold='0Jy', phasecenter='', pblimit=0):
+        #vp = ct.vpmanager()
+        #vp.setpbimage(telescope='HERA', compleximage=pbimage)
+        #vp.saveastable(pbimage + '.tab')
+        spw = '0:{}~{}'.format(start, stop)
+        ctk.tclean(self.msfile, imagename=imagename, cell=cell, imsize=imsize, weighting=weighting, spw=spw, niter=niter, threshold=threshold, phasecenter=phasecenter, pblimit=pblimit)
+        ia = ct.image()
+        ia.open(imagename + '.image')
+        ia.maskhandler('delete', 'mask0')
+        ia.close()
+
+    def to_fits(self, imagename, fitsname, overwrite=False):
+        ctk.exportfits(imagename=imagename, fitsimage=fitsname, overwrite=overwrite)
 
     def remove_casa_image(self, imagename):
         os.system('rm -rf {}.image')
