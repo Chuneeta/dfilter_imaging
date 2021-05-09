0f11c74387de7458186863d2b904ad86888faeb0
Ridhima Nunhokee <ridhima@Ridhimas-MacBook-Pro.local>
2021-05-09 08:48:32 +0500

    added functionality to rephase

--- dfilter_imaging/uvfits_tools.py
+++ dfilter_imaging/uvfits_tools.py
@@ -1,3 +1,4 @@
+from astropy.time import Time
 import pyuvdata
 import casatasks as ctk
 import os, sys
@@ -9,6 +10,13 @@ class UVfits(object):
     def read_uvfits(self):
         self.uvf = pyuvdata.UVData()
         self.uvf.read_uvfits(self.uvfits)
+
+    def phase_to_time(self, time=None):
+        self.read_uvfits()
+        times = self.uvf.time_array
+        phs_time = times[int(len(times)/2.)] if time is None else time
+        print ('Phasing visibilities to {}'.format(phs_time))
+        self.uvf.phase_to_time(Time(phs_time, format='jd', scale='utc'))
 
     def convert_to_ms(self, msname, overwrite=False):
         if overwrite:
